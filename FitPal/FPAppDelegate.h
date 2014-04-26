c1d3aceb82e9821438c91fd0ad5fb05553f100f0
Ahmet Karalar <akaralar@gmail.com>
2014-04-26 16:47:26 +0300

    Initial Commit

---
+++ FitPal/FPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FPAppDelegate.h
+//  FitPal
+//
+//  Created by Ahmet Karalar on 26/4/14.
+//  Copyright (c) 2014 Ahmet Karalar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FitPal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FitPal
+//
+//  Created by Ahmet Karalar on 26/4/14.
+//  Copyright (c) 2014 Ahmet Karalar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FPAppDelegate class]));
+    }
+}
